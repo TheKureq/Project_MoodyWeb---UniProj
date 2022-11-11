b64b1fde5fceb67526597633a6218113f9e3a6da
TheKureq <81649868+TheKureq@users.noreply.github.com>
2022-11-11 18:23:22 +0100

    commit before marging samir php version

--- js/control.js
+++ js/control.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-    $(".led").click(function(){
-        var p = $(this).attr('id');
-        $.get("http://10.0.0.12:80/", {value:p});
+    $(".button").click(function(){
+        var p = $(this).attr('value');
+        $.get("http://10.0.0.12:80/", {select:p});
+    });
+    $(".axis1").click(function(){
+        var p = $(this).attr('value');
+        $.get("http://10.0.0.12:80/", {axis1:p});
+    });
+    $(".axis2").click(function(){
+        var p = $(this).attr('value');
+        $.get("http://10.0.0.12:80/", {axis2:p});
+    });
+    $(".axis3").click(function(){
+        var p = $(this).attr('value');
+        $.get("http://10.0.0.12:80/", {axis3:p});
+    });
+    $(".axis4").click(function(){
+        var p = $(this).attr('value');
+        $.get("http://10.0.0.12:80/", {axis4:p});
+    });
+    $(".axis5").click(function(){
+        var p = $(this).attr('value');
+        $.get("http://10.0.0.12:80/", {axis5:p});
     });
 });
